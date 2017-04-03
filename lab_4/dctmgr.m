72830de7d1ecb054cec6092ab26b90bb8ff9d968
Jay Huang <kingsleyzhijiehuang@gmail.com>
2017-04-02 18:13:42 -0600

    Added lab4

---
+++ lab_4/dctmgr.m
@@ -0,0 +1,21 @@
+function [ output_args ] = Untitled4( input_args )
+%This is assignment 1:
+%Write the MATLAB function dctmgr that implements the following functionality:
+% (a) it takes an luminance (gray-level) image as an input, divides it into non overlapping 8  8
+% blocks, and DCT transform each block according to (1).
+% (b) The DCT coefficients for the entire image are returned into a matrix coeff of size
+% 64  Nblocks, where Nblocks is the number of 8  8 blocks inside the image.
+% (c) For a given block b, the coefficients of in the column coeff(:,b) are organized according
+% to the zig-zag order shown in Fig. 3. That is,
+% coeff(2,b) = F(1,2), coeff(3,b) = F(2,1), coeff(4,b) = F(3,1),...
+% (d) The zero-frequency coefficients, F(1; 1), for each block is encoded using differential
+% encoding,
+% coeff(1,1) = F1(1; 1),
+% coeff(1,2) = F2(1; 1) ? F1(1; 1),
+% coeff(1,3) = F3(1; 1) ? F2(1; 1),
+% ...
+% where Fi(1; 1) is the zero frequency DCT coefficient of block i.
+
+
+end
+
---
+++ lab_4/main.m
@@ -0,0 +1,2 @@
+%This is lab 4 for ECEN 4532 Digital Signal Processing Lab:
+%This is the main script for lab 4
